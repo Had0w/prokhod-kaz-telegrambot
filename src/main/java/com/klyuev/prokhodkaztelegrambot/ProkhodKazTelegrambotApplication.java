e06b0dd3c138d090655c12925a2a204c95a50de4
”Klyuev <11sergey6793@gmail.com>
2021-04-10 19:25:21 +0300

    Create Spring Boot skeleton

---
+++ src/main/java/com/klyuev/prokhodkaztelegrambot/ProkhodKazTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.klyuev.prokhodkaztelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProkhodKazTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProkhodKazTelegrambotApplication.class, args);
+	}
+
+}
